f22d030e448540a80df231c122407d62a7122d20
Marlon Mendoza <marlonmm2003@gmail.com>
2026-08-10 05:56:45 -0500

    fix: apply melee damage to each valid enemy

--- Assets/Scripts/PlayerScripts/Player_Combat.cs
+++ Assets/Scripts/PlayerScripts/Player_Combat.cs
@@ -40,17 +40,16 @@ public class Player_Combat : MonoBehaviour
     {
         Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPoint.position, StatsManager.Instance.weaponRange, enemyLayer);
 
-        foreach (Collider2D enemy in enemies)
+        foreach (Collider2D enemy in CombatTargetResolver.GetDistinctDamageableColliders(enemies))
         {
-            if (enemy.isTrigger) continue;
+            if (!enemy.TryGetComponent(out Enemy_Health enemyHealth) ||
+                !enemy.TryGetComponent(out Enemy_Knockback enemyKnockback))
             {
-                if (enemies.Length > 0)
-                {
-                    enemies[0].GetComponent<Enemy_Health>().ChangeHealth(-StatsManager.Instance.damage);
-                    enemies[0].GetComponent<Enemy_Knockback>().Knockback(transform, StatsManager.Instance.knockbackForce, StatsManager.Instance.knockbackTime, StatsManager.Instance.stunTime);
-                }
+                continue;
             }
 
+            enemyHealth.ChangeHealth(-StatsManager.Instance.damage);
+            enemyKnockback.Knockback(transform, StatsManager.Instance.knockbackForce, StatsManager.Instance.knockbackTime, StatsManager.Instance.stunTime);
         }
     }
 
